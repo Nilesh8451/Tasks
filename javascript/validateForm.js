764c07b9248b2a4a09383dd0c2176e6bee668f8b
Nilesh8451 <nileshchavan8451@gmail.com>
2020-09-16 12:01:39 +0530

    Form Validation Changes

--- javascript/validateForm.js
+++ javascript/validateForm.js
@@ -5,6 +5,9 @@ const email = document.getElementById("inputEmail");
 const password = document.getElementById("inputPassword");
 const rate = document.getElementById("inputRate");
 const check = document.getElementById("inputCheck");
+const gender = document.getElementsByName("inputGender");
+const photo = document.getElementById("profilePhoto");
+const number = document.getElementById("inputNumber");
 
 const nameSpan = document.getElementById("nameSpan");
 const deptSpan = document.getElementById("departSpan");
@@ -13,8 +16,11 @@ const emailSpan = document.getElementById("emailSpan");
 const passwordSpan = document.getElementById("passwordSpan");
 const rateSpan = document.getElementById("rateSpan");
 const checkSpan = document.getElementById("inputCheckSpan");
+const genderSpan = document.getElementById("genderSpan");
+const imageSpan = document.getElementById("imageSpan");
+const numberSpan = document.getElementById("numberSpan");
 
-const deptName = ["computer", "information technology", "civil", "mechanical"];
+const deptName = ["javascript", "java", "dot net", "php"];
 var nameError = 0;
 var emailError = 0;
 var passwordError = 0;
@@ -22,9 +28,14 @@ var ageError = 0;
 var deptError = 0;
 var rateError = 0;
 var checkError = 0;
+var genderError = 0;
+var imageError = 0;
+var numberError = 0;
 
 function validateMe(e) {
-  //   e.preventDefault();
+  //
+
+  // *******************Name Validation*****************************
   if (name.value === "" || name.value.length <= 1 || name.value.length >= 40) {
     nameSpan.textContent = "Name must be min 2 char long and max 40 char long";
     nameError = 1;
@@ -33,6 +44,7 @@ function validateMe(e) {
     nameError = 0;
   }
 
+  // *******************Department Validation*****************************
   if (1) {
     const id = deptName.findIndex((d) => {
       return d === dept.value.toLowerCase();
@@ -47,6 +59,32 @@ function validateMe(e) {
     }
   }
 
+  // *******************Gender Validation*****************************
+  if (!gender[0].checked && !gender[1].checked) {
+    genderError = 1;
+    genderSpan.textContent = "Please select you gender";
+  } else {
+    genderError = 0;
+    genderSpan.textContent = "";
+  }
+
+  // **********************Image Validation**************************
+  // console.log(photo.files["0"]);
+  if (photo.files["0"]) {
+    let size = parseInt(photo.files["0"].size / 1024);
+    if (size > 1024) {
+      imageError = 1;
+      imageSpan.textContent = "Image Size is to large";
+    } else {
+      imageError = 0;
+      imageSpan.textContent = "";
+    }
+  } else {
+    imageError = 1;
+    imageSpan.textContent = "Image is required";
+  }
+
+  // *******************Skill Level Validation*****************************
   if (parseInt(rate.value) < 3) {
     rateError = 1;
     rateSpan.textContent = "Your skills are to low for this Form";
@@ -55,6 +93,7 @@ function validateMe(e) {
     rateSpan.textContent = "";
   }
 
+  // *******************Age Validation*****************************
   if (age.value !== "") {
     console.log(age.value);
     const ageArray = age.value.split("-");
@@ -71,6 +110,7 @@ function validateMe(e) {
     ageError = 1;
   }
 
+  // *******************Email Validation*****************************
   if (email.value !== "") {
     regex = /^[A-Za-z0-9+_.-]+@(.+)$/;
     if (!regex.test(email.value)) {
@@ -87,6 +127,25 @@ function validateMe(e) {
     emailError = 1;
   }
 
+  // *******************Number Validation*************************
+
+  if (number.value) {
+    console.log(number.value);
+    var phoneno = /^\d{10}$/;
+
+    if (number.value.match(phoneno)) {
+      numberSpan.textContent = "";
+      numberError = 0;
+    } else {
+      numberSpan.textContent = "Please enter valid number";
+      numberError = 1;
+    }
+  } else {
+    numberError = 1;
+    numberSpan.textContent = "Please Enter Your mobile Number";
+  }
+
+  // *******************Password Validation*****************************
   if (password.value === "" || password.value.length <= 5) {
     // console.log("Password must be at least 6 char long");
     passwordSpan.textContent = "Password must be at least 6 char long";
@@ -96,7 +155,7 @@ function validateMe(e) {
     passwordError = 0;
   }
 
-  // console.log(check.checked);
+  // *******************Terms and Condition Validation*****************************
   if (!check.checked) {
     checkError = 1;
     checkSpan.textContent = "Please Accept Terms and Condition";
@@ -112,7 +171,9 @@ function validateMe(e) {
     emailError ||
     deptError ||
     rateError ||
-    checkError
+    checkError ||
+    genderError ||
+    numberError
   ) {
     return false;
   }
